32be4807964d9841b59b34396b5dce4ea56cb14b
mbaltman <meggersa@gmail.com>
2022-09-04 01:28:13 -0400

    Created Level Manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    private List<Vector3Int> bloodDrip_coord;
+    private List<Vector3Int> box_coord;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void LevelManager( int level)
+    {
+      
     }
 }
