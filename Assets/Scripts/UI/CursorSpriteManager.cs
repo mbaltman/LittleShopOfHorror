f0d404de73c20289b4003b1c4b0a62abba7616a8
mbaltman <meggersa@gmail.com>
2022-11-17 19:18:25 -0500

    Mouse Display
    Corrected Display of mouse image. Made it so that clickable tiles appear clickable.
    
    Cursor object is used, so cursor images do not scale with the rest of the UI.

---
+++ Assets/Scripts/UI/CursorSpriteManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CursorSpriteManager : MonoBehaviour
+{
+    public Texture2D cursor_click;
+    public Texture2D cursor_normal;
+    Vector2 hotspot = new Vector2(30,30);
+    
+    void Awake()
+    {
+        Cursor.SetCursor(cursor_normal, hotspot, CursorMode.Auto);
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+    }
+    void OnMouseEnter()
+    {
+        Debug.Log("enter");
+        Cursor.SetCursor(cursor_click, hotspot, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("exit");
+        Cursor.SetCursor(cursor_normal, hotspot, CursorMode.Auto);
+    }
+
+     private void ChangedActiveScene(Scene current, Scene next)
+    {
+       Cursor.SetCursor(cursor_normal, hotspot, CursorMode.Auto);
+    }
+
+}
